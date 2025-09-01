2daf4de93de193b2efad4ab4150dbb92e43ebe0d
Charles Sarpong <charlessarpong9509@gmail.com>
2025-08-31 22:54:56 -0400

    Added hobby question to input script

--- 01-read-input_copy_cli.sh
+++ 01-read-input_copy_cli.sh
@@ -8,3 +8,8 @@ echo 'Thanks Steven! I see you are ' $age ' years old'
 echo "What school do you currently attend?"
 read school
 echo 'Nice! ' $school ' sounds like a great place to learn.'
+echo "What’s your favorite hobby?"
+read hobby
+echo "That’s awesome! $hobby sounds like a great way to spend time."
+
+
